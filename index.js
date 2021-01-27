616b1b03d516d033300ec6e5507ac229725c8d46
reggev <reggev.am@gmail.com>
2021-01-27 20:59:44 +0200

    splitted commit and push commands, fixed exports

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 module.exports.createFileMeta = require('./lib/createFileMeta');
 module.exports.DvcClient = require('./lib/DvcClient');
 module.exports.filePath = require('./lib/filePath');
-module.exports.glob = require('./lib/filePath');
 module.exports.glob = require('./lib/glob');
 module.exports.Repository = require('./lib/Repository');
 module.exports.Shell = require('./lib/Shell');
