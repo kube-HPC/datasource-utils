7e1773f42f407c1c6801229d25dece926ea50b46
reggev <reggev.am@gmail.com>
2021-01-27 19:32:05 +0200

    unified merhods from service and cli

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-module.exports = require('./lib/storage-manager');
+module.exports.createFileMeta = require('./lib/createFileMeta');
+module.exports.DvcClient = require('./lib/DvcClient');
+module.exports.filePath = require('./lib/filePath');
+module.exports.glob = require('./lib/filePath');
+module.exports.glob = require('./lib/glob');
+module.exports.Repository = require('./lib/Repository');
+module.exports.Shell = require('./lib/Shell');
+
+/** @typedef {import('./lib/types').FileMeta} FileMeta */
+/** @typedef {import('./lib/types').LocalFileMeta} LocalFileMeta */
+/** @typedef {import('./lib/types').DvcFileMeta} DvcFileMeta */
+/** @typedef {import('./lib/types').DvcContent} DvcContent */
--- lib/types.d.ts
+++ lib/types.d.ts
@@ -1,5 +1,12 @@
-export type NormalizedFileMeta = { [fileId: string]: FileMeta };
-export type MulterFile = Express.Multer.File;
-export type FileMeta = import('@hkube/db/lib/DataSource').FileMeta;
-export type SourceTargetArray = [FileMeta, FileMeta];
-export type config = typeof import('./../../config/main/config.base');
+import type { FileMeta } from '@hkube/db/lib/DataSource';
+
+export type LocalFileMeta = Omit<FileMeta, 'path'>;
+
+export type DvcFileMeta = LocalFileMeta & { hash: string };
+
+export type DvcContent = {
+    outs: { md5: string; size: number; path: string }[];
+    meta?: {
+        hkube?: DvcFileMeta;
+    };
+};
